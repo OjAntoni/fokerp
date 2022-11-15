f23b7958d2ceb870bf61c35476247fbd52e90e91
OjAntoni <zezyulin90@gmail.com>
2022-11-15 09:38:29 +0100

    Initial commit

---
+++ src/test/java/pw/edu/pl/AppTest.java
@@ -0,0 +1,20 @@
+package pw.edu.pl;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
